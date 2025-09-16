f7f154a84f559863c00d9c8a412d877b89058bf0
eduardo239 <eucrieiumaconta@gmail.com>
2025-09-16 14:55:01 -0300

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "Project ID"
+  type = string
+  default = "learn-gcp-terraform-469711"
+}
+
+variable "region" {
+  description = "GCP region"
+  type = string
+  default = "us-central1"
+}
