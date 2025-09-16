d9ed72470446a5074555d844bf4e564cd2f94fec
eduardo239 <eucrieiumaconta@gmail.com>
2025-09-16 19:06:42 -0300

    Removido backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
-terraform {
-  backend "gcs" {
-    bucket  = "learn-gcp-terraform-469711-terraform-state"
-    prefix  = "terraform/state"
-  }
-}
+# terraform {
+#   backend "gcs" {
+#     bucket  = "learn-gcp-terraform-469711-terraform-state"
+#     prefix  = "terraform/state"
+#   }
+# }
